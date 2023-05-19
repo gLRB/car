4e7bddefdcab070177552b853678405009886ad7
alan <alan@192.168.10.97>
2023-05-20 01:10:02 +0800

    new prod config

--- cars-platform/backend/conf/config.go
+++ cars-platform/backend/conf/config.go
@@ -7,13 +7,7 @@ func GetCurEnv() string {
 }
 
 func GetDBConf() string {
-	if GetCurEnv() == "local" {
-		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local&timeout=%s", LocalUserName, LocalPassWord, LocalHost, LocalPort, DBName, Timeout)
-		//fmt.Print("dsn", dsn)
-		return dsn
-	} else {
-		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local&timeout=%s", ProdUserName, ProdPassWord, ProdHost, ProdPort, DBName, Timeout)
-		//fmt.Print("dsn", dsn)
-		return dsn
-	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local&timeout=%s", LocalUserName, LocalPassWord, LocalHost, LocalPort, DBName, Timeout)
+	//fmt.Print("dsn", dsn)
+	return dsn
 }
